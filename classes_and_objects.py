bd825eee01784d9bb4605be59227355f039e30a3
Guntupalli <guntupab@hyd-guntupab-m.ds.ad.adp.com>
2021-02-03 11:40:20 +0530

    made adjustments on student address instance variable

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -1,16 +1,16 @@
 class Student:
     college = ''
 
-    def __init__(self, name, age=None, marks=None, add=None):
+    def __init__(self, name, age=None, marks=None, add):
         self.name = name
         self.age = age
         self.marks = marks
-        self.add = add
+        self.add = 'Florida'
 
     def display(self):
         return '##### {} - College #####\nName: {}\nAge: {}\nMarks: {}\nAddress: {}\n'.format(self.college, self.name, self.age, self.marks, self.add)
 
 
 college = Student.college = 'VBIR'
-s1 = Student('Ramu', 20, 900, 'Florida')
+s1 = Student('Ramu', 20, 900,)
 print(s1.display())
