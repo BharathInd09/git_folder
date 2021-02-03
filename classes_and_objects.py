12084ca750da6cddd5e568b55c72ee0ae43b54a7
BharathInd09 <64138425+BharathInd09@users.noreply.github.com>
2021-02-03 10:44:00 +0530

    Update classes_and_objects.py

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -1,15 +1,16 @@
 class Student:
     college = ''
 
-    def __init__(self, name, age=None, marks=None):
+    def __init__(self, name, age=None, marks=None, add=None):
         self.name = name
         self.age = age
         self.marks = marks
+        self.add = add
 
     def display(self):
-        return '##### {} - College #####\nName: {}\nAge: {}\nMarks: {}\n'.format(self.college, self.name, self.age, self.marks)
+        return '##### {} - College #####\nName: {}\nAge: {}\nMarks: {}\nAddress: {}\n'.format(self.college, self.name, self.age, self.marks, self.add)
 
 
 college = Student.college = 'VBIR'
-s1 = Student('Ramu', 20, 900)
+s1 = Student('Ramu', 20, 900, 'Florida')
 print(s1.display())
